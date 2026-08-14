0c5657e1bdaca844fb932d43f3086428a5fbdc8c
amul087 <darshuimage@gmail.com>
2026-08-15 04:00:48 +0530

    Fix CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://shy-vizuals.vercel.app",
+    ],
     credentials: true,
   })
 );
